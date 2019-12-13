9e7bd7403138c6fee7fec4fd4cab213399d4388a
Hazurl <ricoujules69@gmail.com>
2019-12-13 18:43:52 +0100

    Edge Quadtree working

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 #include <variant>
 
 enum class Mode {
-    Tiles, Collision, Test
+    Tiles, Collision, Test, SIZE
 };
 
 struct Segment {
@@ -70,27 +70,64 @@ std::optional<sf::Vector2f> intersection(Segment const& lhs, Segment const& rhs)
     return p + t * r;
 }
 
+std::optional<sf::Vector2f> intersection_with_x(float x, Segment const& lhs) {
+    if (lhs.from.x < x && lhs.to.x < x) {
+        return std::nullopt;
+    }
+
+    if (lhs.from.x > x && lhs.to.x > x) {
+        return std::nullopt;
+    }
+
+    auto d = lhs.to - lhs.from;
+    if(d.x == 0.f) {
+        return std::nullopt;
+    }
+
+    auto t = (x - lhs.from.x) / d.x;
+    auto y = lhs.from.y + t * d.y;
+    return sf::Vector2f{ x, y };
+}
+
+std::optional<sf::Vector2f> intersection_with_y(float y, Segment const& lhs) {
+    if (lhs.from.y < y && lhs.to.y < y) {
+        return std::nullopt;
+    }
+
+    if (lhs.from.y > y && lhs.to.y > y) {
+        return std::nullopt;
+    }
+
+    auto d = lhs.to - lhs.from;
+    if(d.y == 0.f) {
+        return std::nullopt;
+    }
+
+    auto t = (y - lhs.from.y) / d.y;
+    auto x = lhs.from.x + t * d.x;
+    return sf::Vector2f{ x, y };
+}
+
 struct Tree;
 
 struct Leaf {
-    std::vector<std::size_t> lines;
+    std::vector<Segment> lines;
 
-    void push_line(Segment const& line, std::size_t id) {
-        std::cout << line.from.x << ", " << line.from.y << " <-> " << line.to.x << ", " << line.to.y << " with #" << id << " added to " << this << '\n';
-        lines.push_back(id);
+    void push_line(Segment const& line, std::size_t) {
+        lines.push_back(line);
     }
 
 };
 
 struct Node {
-    std::variant<Leaf, std::unique_ptr<Tree>> value; 
+    unsigned char max_depth = 10;
+    std::variant<Leaf, std::unique_ptr<Tree>> value = Leaf{}; 
 
     void push_line(Segment const& line, std::size_t id);
 };
 
 struct Tree {
-    Segment vertical_separator;
-    Segment horizontal_separator;
+    sf::Vector2f separators;
 
     Node top_left;
     Node top_right;
@@ -98,21 +135,21 @@ struct Tree {
     Node bottom_right;
 
     void push_line(Segment const& line, std::size_t id) {
-        auto cv = intersection(vertical_separator, line);
-        auto ch = intersection(horizontal_separator, line);
+        auto cv = intersection_with_x(separators.x, line);
+        auto ch = intersection_with_y(separators.y, line);
 
         if (cv && ch) {
-            if (line.from.x < vertical_separator.from.x) {
-                if (line.from.y < horizontal_separator.from.y) {
+            if (line.from.x < separators.x) {
+                if (line.from.y < separators.y) {
                     // top left to bottom right
-                    if (ch->x < vertical_separator.from.x) { // pass by bottom left
+                    if (ch->x < separators.x) { // pass by bottom left
                         Segment line1{ line.from, *ch };
                         Segment line2{ *ch, *cv };
                         Segment line3{ *cv, line.to };
                         top_left.push_line(line1, id);
                         bottom_left.push_line(line2, id);
                         bottom_right.push_line(line3, id);
-                    } else if (ch->x > vertical_separator.from.x) { // pass by top right
+                    } else if (ch->x > separators.x) { // pass by top right
                         Segment line1{ line.from, *cv };
                         Segment line2{ *cv, *ch };
                         Segment line3{ *ch, line.to };
@@ -127,14 +164,14 @@ struct Tree {
                     }
                 } else {
                     // bottom left to top right
-                    if (ch->x < vertical_separator.from.x) { // pass by top left
+                    if (ch->x < separators.x) { // pass by top left
                         Segment line1{ line.from, *ch };
                         Segment line2{ *ch, *cv };
                         Segment line3{ *cv, line.to };
                         bottom_left.push_line(line1, id);
                         top_left.push_line(line2, id);
                         top_right.push_line(line3, id);
-                    } else if (ch->x > vertical_separator.from.x) { // pass by bottom right
+                    } else if (ch->x > separators.x) { // pass by bottom right
                         Segment line1{ line.from, *cv };
                         Segment line2{ *cv, *ch };
                         Segment line3{ *ch, line.to };
@@ -149,16 +186,16 @@ struct Tree {
                     }
                 }
             } else {
-                if (line.from.y < horizontal_separator.from.y) {
+                if (line.from.y < separators.y) {
                     // top right to bottom left
-                    if (ch->x < vertical_separator.from.x) { // pass by top left
+                    if (ch->x < separators.x) { // pass by top left
                         Segment line1{ line.from, *cv };
                         Segment line2{ *cv, *ch };
                         Segment line3{ *ch, line.to };
                         top_right.push_line(line1, id);
                         top_left.push_line(line2, id);
                         bottom_left.push_line(line3, id);
-                    } else if (ch->x > vertical_separator.from.x) { // pass by bottom right 
+                    } else if (ch->x > separators.x) { // pass by bottom right 
                         Segment line1{ line.from, *ch };
                         Segment line2{ *ch, *cv };
                         Segment line3{ *cv, line.to };
@@ -173,14 +210,14 @@ struct Tree {
                     }
                 } else {
                     // bottom right to top left
-                    if (ch->x < vertical_separator.from.x) { // pass by bottom left
+                    if (ch->x < separators.x) { // pass by bottom left
                         Segment line1{ line.from, *cv };
                         Segment line2{ *cv, *ch };
                         Segment line3{ *ch, line.to };
                         bottom_right.push_line(line1, id);
                         bottom_left.push_line(line2, id);
                         top_left.push_line(line3, id);
-                    } else if (ch->x > vertical_separator.from.x) { // pass by top right
+                    } else if (ch->x > separators.x) { // pass by top right
                         Segment line1{ line.from, *ch };
                         Segment line2{ *ch, *cv };
                         Segment line3{ *cv, line.to };
@@ -199,8 +236,8 @@ struct Tree {
             Segment line1{ line.from, *cv };
             Segment line2{ *cv, line.to };
 
-            if (line.from.y < horizontal_separator.from.y) {
-                if (line.from.x < vertical_separator.from.x) {
+            if (line.from.y < separators.y) {
+                if (line.from.x < separators.x) {
                     top_left.push_line(line1, id);
                     top_right.push_line(line2, id);
                 } else {
@@ -208,7 +245,7 @@ struct Tree {
                     top_right.push_line(line1, id);
                 }
             } else {
-                if (line.from.x < vertical_separator.from.x) {
+                if (line.from.x < separators.x) {
                     bottom_left.push_line(line1, id);
                     bottom_right.push_line(line2, id);
                 } else {
@@ -219,8 +256,8 @@ struct Tree {
         } else if (ch) {
             Segment line1{ line.from, *ch };
             Segment line2{ *ch, line.to };
-            if (line.from.x < vertical_separator.from.x) {
-                if (line.from.y < horizontal_separator.from.y) {
+            if (line.from.x < separators.x) {
+                if (line.from.y < separators.y) {
                     top_left.push_line(line1, id);
                     bottom_left.push_line(line2, id);
                 } else {
@@ -228,7 +265,7 @@ struct Tree {
                     bottom_left.push_line(line1, id);
                 }
             } else {
-                if (line.from.y < horizontal_separator.from.y) {
+                if (line.from.y < separators.y) {
                     top_right.push_line(line1, id);
                     bottom_right.push_line(line2, id);
                 } else {
@@ -237,14 +274,14 @@ struct Tree {
                 }
             }
         } else {
-            if (line.from.x < vertical_separator.from.x) {
-                if (line.from.y < horizontal_separator.from.y) {
+            if (line.from.x < separators.x) {
+                if (line.from.y < separators.y) {
                     top_left.push_line(line, id);
                 } else {
                     bottom_left.push_line(line, id);
                 }
             } else {
-                if (line.from.y < horizontal_separator.from.y) {
+                if (line.from.y < separators.y) {
                     top_right.push_line(line, id);
                 } else {
                     bottom_right.push_line(line, id);
@@ -256,7 +293,28 @@ struct Tree {
 
 void Node::push_line(Segment const& line, std::size_t id) {
     if (auto leaf = std::get_if<Leaf>(&value)) {
-        leaf->push_line(line, id);
+        if (leaf->lines.size() >= 4 && max_depth > 0) {
+            auto tree = std::make_unique<Tree>();
+            tree->separators = line.from + line.to;
+            for(auto const& l : leaf->lines) {
+                tree->separators += l.from + l.to;
+            }
+
+            tree->separators.x /= leaf->lines.size() * 2 + 2;
+            tree->separators.y /= leaf->lines.size() * 2 + 2;
+
+            for(auto const& l : leaf->lines) {
+                tree->push_line(l, 0);
+            }
+            tree->push_line(line, 0);
+            tree->top_left.max_depth = max_depth - 1;
+            tree->top_right.max_depth = max_depth - 1;
+            tree->bottom_left.max_depth = max_depth - 1;
+            tree->bottom_right.max_depth = max_depth - 1;
+            value = std::move(tree);
+        } else {
+            leaf->push_line(line, id);
+        }
     } else {
         auto& tree = std::get<std::unique_ptr<Tree>>(value);
         tree->push_line(line, id);
@@ -291,6 +349,74 @@ struct QuadTree {
     }
 
 };
+
+void walk(sf::RenderTarget& target, sf::FloatRect bounds, Node const& node, std::size_t depth = 0) {
+    static std::array colors {
+        sf::Color::Magenta,
+        sf::Color::Cyan,
+        sf::Color::Yellow,
+    };
+    if (std::holds_alternative<Leaf>(node.value)) return;
+
+    auto const& tree = *std::get<std::unique_ptr<Tree>>(node.value);
+
+    std::array separators{
+        sf::Vertex({ tree.separators.x, bounds.top }, colors[depth % colors.size()]),
+        sf::Vertex({ tree.separators.x, bounds.top + bounds.height }, colors[depth % colors.size()]),
+        sf::Vertex({ bounds.left, tree.separators.y }, colors[depth % colors.size()]),
+        sf::Vertex({ bounds.left + bounds.width, tree.separators.y }, colors[depth % colors.size()])
+    };
+
+    target.draw(separators.data(), separators.size(), sf::PrimitiveType::Lines);
+
+    float bx = bounds.left;
+    float sx = tree.separators.x;
+    float ex = bounds.left + bounds.width;
+
+    float by = bounds.top;
+    float sy = tree.separators.y;
+    float ey = bounds.top + bounds.height;
+
+    sf::FloatRect top_left_bounds(
+        bx, by, 
+        sx - bx, sy - by);
+    sf::FloatRect top_right_bounds(
+        sx, by, 
+        ex - sx, sy - by);
+    sf::FloatRect bottom_left_bounds(
+        bx, sy, 
+        sx - bx, ey - sy);
+    sf::FloatRect bottom_right_bounds(
+        sx, sy, 
+        ex - sx, ey - sy);
+
+    walk(target, top_left_bounds, tree.top_left, depth + 1);
+    walk(target, top_right_bounds, tree.top_right, depth + 1);
+    walk(target, bottom_left_bounds, tree.bottom_left, depth + 1);
+    walk(target, bottom_right_bounds, tree.bottom_right, depth + 1);
+}
+
+Leaf& find_leaf_with_point(Node& node, sf::Vector2f const& p) {
+    if (auto leaf = std::get_if<Leaf>(&node.value)) {
+        return *leaf;
+    }
+
+    auto& tree = *std::get<std::unique_ptr<Tree>>(node.value);
+
+    if (tree.separators.x > p.x) {
+        if (tree.separators.y > p.y) {
+            return find_leaf_with_point(tree.top_left, p);
+        } else {
+            return find_leaf_with_point(tree.bottom_left, p);
+        }
+    } else {
+        if (tree.separators.y > p.y) {
+            return find_leaf_with_point(tree.top_right, p);
+        } else {
+            return find_leaf_with_point(tree.bottom_right, p);
+        }
+    }
+}
 
 
 int main(int argc, char** argv) {
@@ -377,27 +503,14 @@ int main(int argc, char** argv) {
 
     std::vector<sf::Vertex> all_lines;
 
-    Tree tree;
-    std::cout << "TOP LEFT IS " << &tree.top_left << '\n';
-    std::cout << "TOP RIGHT IS " << &tree.top_right << '\n';
-    std::cout << "BOTTOM LEFT IS " << &tree.bottom_left << '\n';
-    std::cout << "BOTTOM RIGHT IS " << &tree.bottom_right << '\n';
-
-    tree.vertical_separator = {
-        separators[0].position,
-        separators[1].position
-    };
-    tree.horizontal_separator = {
-        separators[2].position,
-        separators[3].position
-    };
+    Node quadtree_root;
 
     while(window.isOpen()) {
         sf::Event event;
         while(window.pollEvent(event)) {
             if (event.type == sf::Event::KeyPressed) {
                 if (event.key.code == sf::Keyboard::M) {
-                    mode = mode == Mode::Tiles ? Mode::Collision : Mode::Tiles;
+                    mode = static_cast<Mode>((static_cast<std::underlying_type_t<Mode>>(mode) + 1) % static_cast<std::underlying_type_t<Mode>>(Mode::SIZE));
                     continue;
                 }
             }
@@ -500,7 +613,7 @@ int main(int argc, char** argv) {
                             current_line[1].position = { event.mouseButton.x, event.mouseButton.y };
                             all_lines.emplace_back(current_line[0]).color = sf::Color::Blue;
                             all_lines.emplace_back(current_line[1]).color = sf::Color::Blue;
-                            tree.push_line({ current_line[0].position, current_line[1].position }, 0);
+                            quadtree_root.push_line({ current_line[0].position, current_line[1].position }, 0);
                         }
                     } else if (event.type == sf::Event::MouseWheelScrolled) {
                     } else if (event.type == sf::Event::MouseMoved) {
@@ -548,7 +661,18 @@ int main(int argc, char** argv) {
                     window.draw(current_line.data(), current_line.size(), sf::PrimitiveType::Lines);
                 }
                 window.draw(all_lines.data(), all_lines.size(), sf::PrimitiveType::Lines);
-                window.draw(separators.data(), separators.size(), sf::PrimitiveType::Lines);
+                walk(window, sf::FloatRect(0, 0, window.getSize().x, window.getSize().y), quadtree_root);
+
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::I)) {
+                    auto& leaf = find_leaf_with_point(quadtree_root, window.mapPixelToCoords(sf::Mouse::getPosition(window)));
+                    std::vector<sf::Vertex> vertices(leaf.lines.size() * 2, sf::Vertex({}, sf::Color::Green));
+                    auto it = std::begin(vertices);
+                    for(auto const& l : leaf.lines) {
+                        (it++)->position = l.from;
+                        (it++)->position = l.to;
+                    }
+                    window.draw(vertices.data(), vertices.size(), sf::PrimitiveType::Lines);
+                }
                 break;
             }
         }
